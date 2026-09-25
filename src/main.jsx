f71a802765e2635fe3f23cbfefa42bbc0230a373
Daniel García <ddge07@gmail.com>
2026-09-25 11:49:09 -0500

    refactor: wire portfolio theme provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { PortfolioThemeProvider } from './theme/PortfolioThemeProvider.jsx'
 import './styles.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <PortfolioThemeProvider>
+      <App />
+    </PortfolioThemeProvider>
   </StrictMode>,
 )
